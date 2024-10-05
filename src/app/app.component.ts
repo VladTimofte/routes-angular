565543922f37fdd3fafb88f27cf4898df90f43b5
VladTimofte <vladtimofte999@gmail.com>
2024-10-05 15:09:11 +0300

    init routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(private router: Router) {
+
+  }
+
+  navigateTo(routeParam: string) {
+    console.log('Navigheaza catre ruta: ', routeParam)
+    this.router.navigateByUrl(routeParam)
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from '@angular/router';
+import { AngajatiComponent } from './components/angajati/angajati.component';
+import { BugeteComponent } from './components/bugete/bugete.component';
+import { ResurseComponent } from './components/resurse/resurse.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'angajati', component: AngajatiComponent},
+  {path: 'bugete', component: BugeteComponent},
+  {path: 'resurse', component: ResurseComponent},
+  {path: '**', component: NotFoundComponent}
   ];
